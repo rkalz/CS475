8b71da12ff4c47dd9561676932f6e0b238ce8cff
Rofael Aleezada <me@rofael.com>
2019-04-20 14:27:24 -0500

    Added weekday

--- final/csv_to_json.py
+++ final/csv_to_json.py
@@ -52,9 +52,27 @@ def get_times():
     with open("times.json", 'w') as f:
         json.dump(fixed_rows, f)
 
+def get_weekday():
+    rows = []
+    with open("export/day_of_week.csv", "rt", encoding="utf16") as f:
+        for row in f:
+            rows.append(row.split())
+    rows.pop(0)
+
+    fixed_rows = []
+    for row in rows:
+        fixed_rows.append({
+            "weekday": row[0],
+            "count": int(row[1])
+        })
+
+    with open("day_of_week.json", 'w') as f:
+        json.dump(fixed_rows, f)
+
 if __name__ == "__main__":
     # get_dates()
     # get_sankey("likes")
     # get_sankey("mentions")
 
-    get_times()
+    # get_times()
+    get_weekday()
