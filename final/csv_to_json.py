4849efabb813fbfc9cc3978520e7d532b1243fb9
Rofael Aleezada <me@rofael.com>
2019-04-20 14:15:09 -0500

    Added time bar graph

--- final/csv_to_json.py
+++ final/csv_to_json.py
@@ -35,7 +35,26 @@ def get_sankey(name):
     with open(name + '.json', 'w') as f:
         json.dump(fixed_rows, f)
 
+def get_times():
+    rows = []
+    with open("export/times.csv", "rt", encoding="utf16") as f:
+        for row in f:
+            rows.append(row.split())
+    rows.pop(0)
+
+    fixed_rows = []
+    for row in rows:
+        fixed_rows.append({
+            "hour": int(row[0]),
+            "count": int(row[1])
+        })
+
+    with open("times.json", 'w') as f:
+        json.dump(fixed_rows, f)
+
 if __name__ == "__main__":
-    get_dates()
-    get_sankey("likes")
-    get_sankey("mentions")
+    # get_dates()
+    # get_sankey("likes")
+    # get_sankey("mentions")
+
+    get_times()
